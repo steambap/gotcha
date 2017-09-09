c211529cdf60ba5f8b494f6fc0f38e8c27a04f44
Weilin Shi <934587911@qq.com>
2017-09-09 11:25:16 +0800

    update: gitignore add: more estree type

--- ast.go
+++ ast.go
@@ -29,13 +29,13 @@ type Identifier struct {
 	name string
 }
 
-func (ident *Identifier) Pattern() Node {
-	return ident.Node
-}
+func (*Identifier) Pattern() {}
 
-func (ident *Identifier) Expression() Node {
-	return ident.Node
-}
+func (*Identifier) Expression() {}
+
+func (*Identifier) forStatementInit() {}
+
+func (*Identifier) forInStatementLeft() {}
 
 // interface Literal <: Expression
 type Literal struct {
@@ -43,9 +43,7 @@ type Literal struct {
 	Value interface{}
 }
 
-func (literal *Literal) Expression() Node {
-	return literal.Node
-}
+func (*Literal) Expression() {}
 
 type Regex struct {
 	Pattern string
@@ -74,17 +72,16 @@ type Function struct {
 
 // interface Statement <: Node
 type Statement interface {
-	Statement() Node
+	Statement()
 }
 
 // interface ExpressionStatement <: Statement
 type ExpressionStatement struct {
 	Node
+	Expression Expression
 }
 
-func (exprStmt *ExpressionStatement) Statement() Node {
-	return exprStmt.Node
-}
+func (exprStmt *ExpressionStatement) Statement() {}
 
 // interface Directive <: ExpressionStatement
 type Directive struct {
@@ -98,9 +95,7 @@ type BlockStatement struct {
 	Body []Statement
 }
 
-func (blockStmt *BlockStatement) Statement() Node {
-	return blockStmt.Node
-}
+func (*BlockStatement) Statement() {}
 
 // interface FunctionBody <: BlockStatement
 type FunctionBody struct {
@@ -112,23 +107,192 @@ type EmptyStatement struct {
 	Node
 }
 
-func (emptyStmt *EmptyStatement) Statement() Node {
-	return emptyStmt.Node
-}
+func (*EmptyStatement) Statement() {}
 
 // interface DebuggerStatement <: Statement
 type DebuggerStatement struct {
 	Node
 }
 
-func (debuggerStmt *DebuggerStatement) Statement() Node {
-	return debuggerStmt.Node
+func (*DebuggerStatement) Statement() {}
+
+// interface WithStatement <: Statement
+type WithStatement struct {
+	Node
+	Object Expression
+	Body Statement
+}
+
+func (*WithStatement) Statement() {}
+
+// interface ReturnStatement <: Statement
+type ReturnStatement struct {
+	Node
+	Argument *Expression
+}
+
+func (*ReturnStatement) Statement() {}
+
+// interface LabeledStatement <: Statement
+type LabeledStatement struct {
+	Node
+	Label Identifier
+	Body Statement
+}
+
+func (*LabeledStatement) Statement() {}
+
+// interface BreakStatement <: Statement
+type BreakStatement struct {
+	Node
+	Label *Identifier
+}
+
+func (*BreakStatement) Statement() {}
+
+// interface ContinueStatement <: Statement
+type ContinueStatement struct {
+	Node
+	Label *Identifier
+}
+
+func (*ContinueStatement) Statement() {}
+
+// interface IfStatement <: Statement
+type IfStatement struct {
+	Node
+	Test Expression
+	Consequent Statement
+	Alternate *Statement
+}
+
+func (*IfStatement) Statement() {}
+
+// interface SwitchStatement <: Statement
+type SwitchStatement struct {
+	Node
+	Discriminant Expression
+	Cases []SwitchCase
+}
+
+func (*SwitchStatement) Statement() {}
+
+// interface SwitchCase <: Node
+type SwitchCase struct {
+	Node
+	Test *Expression
+	Consequent []Statement
+}
+
+// interface ThrowStatement <: Statement
+type ThrowStatement struct {
+	Node
+	Argument Expression
+}
+
+func (*ThrowStatement) Statement() {}
+
+// interface TryStatement <: Statement
+type TryStatement struct {
+	Node
+	Block BlockStatement
+	Handler *CatchClause
+	Finalizer *BlockStatement
+}
+
+func (*TryStatement) Statement() {}
+
+// interface CatchClause <: Node
+type CatchClause struct {
+	Node
+	Param Pattern
+	Body BlockStatement
+}
+
+// interface WhileStatement <: Statement
+type WhileStatement struct {
+	Node
+	Test Expression
+	Body Statement
+}
+
+func (*WhileStatement) Statement() {}
+
+// interface DoWhileStatement <: Statement
+type DoWhileStatement struct {
+	Node
+	Test Expression
+	Body Statement
+}
+
+func (*DoWhileStatement) Statement() {}
+
+type forStatementInit interface {
+	forStatementInit()
+}
+
+// interface ForStatement <: Statement
+type ForStatement struct {
+	Node
+	Init *forStatementInit
+	Test *Expression
+	Update *Expression
+	Body Statement
+}
+
+func (*ForStatement) Statement() {}
+
+type forInStatementLeft interface {
+	forInStatementLeft()
+}
+
+// interface ForInStatement <: Statement
+type ForInStatement struct {
+	Node
+	Left forInStatementLeft
+	Right Expression
+	Body Statement
+}
+
+func (*ForInStatement) Statement() {}
+
+type Declaration interface {
+	Statement
+}
+
+// interface FunctionDeclaration <: Function, Declaration
+type FunctionDeclaration struct {
+	Function
+}
+
+func (*FunctionDeclaration) Statement() {}
+
+// interface VariableDeclaration <: Declaration
+type VariableDeclaration struct {
+	Node
+	Declarations []VariableDeclarator
+	Kind string
+}
+
+func (*VariableDeclaration) Statement() {}
+
+func (*VariableDeclaration) forStatementInit() {}
+
+func (*VariableDeclaration) forInStatementLeft() {}
+
+// interface VariableDeclarator <: Node
+type VariableDeclarator struct {
+	Node
+	Id Pattern
+	Init *Expression
 }
 
 type Expression interface {
-	Expression() Node
+	forStatementInit
+	Expression()
 }
 
 type Pattern interface {
-	Pattern() Node
+	forInStatementLeft
+	Pattern()
 }
